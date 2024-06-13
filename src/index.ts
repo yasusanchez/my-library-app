7c11bad6bbfd7df6af8494fe5e466c779235ec1f
Yasuri <sanchezyasuri475@gmail.com>
2024-06-13 06:59:50 -0500

    setup inicial proyecto en typescript

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+/* Este fragmento de código utiliza una expresión de función de invocación inmediata (IIFE) en
+TypeScript. */
+(()=> {
+    /* El código declara una variable constante `myName` de tipo `string` con el valor ``Nicolas''. Luego,
+    registra el valor de la variable `myName` en la consola. Todo el código está incluido en una
+    expresión de función invocada inmediatamente (IIFE) en TypeScript, lo que significa que la función
+    se ejecuta inmediatamente después de definirse. */
+        const myName: string = 'Nicolas';
+        console.log(myName);
+    })();
