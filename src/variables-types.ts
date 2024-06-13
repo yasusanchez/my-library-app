839503deb9b62281deed894fa197f9cd989701a4
Yasuri <sanchezyasuri475@gmail.com>
2024-06-13 07:44:39 -0500

    tipado primitivo

---
+++ src/variables-types.ts
@@ -0,0 +1,22 @@
+let bookTitle: string = "TypeScript Basics";
+const libraryName: string = "My Library";
+let numberOfBooks: number = 250;
+let isLibraryOpen: boolean = true;
+
+//Libro
+let titulo: string = "TypeScript Essentials";
+let autor: string = "John Doe";
+let genero: string = "Ciencia Ficción";
+let idioma: string = "Español";
+let precio: number = 29.99;
+let formato: string = "Tapa Dura";
+let isbn: string = "978-3-16-148410-0";
+let descripcion: string = "Un libro esencial para aprender TypeScript.";
+let estado: string = "Nuevo";
+let ubicacion: string = "Sección A3, Estante 4";
+let fecha_publicacion: string = "2024-06-01";
+let editorial: string = "Tech Books Publishing";
+let paginas: number = 350;
+let dimensiones: number = 15.2; //cm
+let peso: number = 0.5; //kilogramos
+let disponible: boolean = true; 
